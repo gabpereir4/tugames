1567a49a98f4f5d91e3407f18d5b103ea435d787
gabpereir4 <a222101@epci.online.pt>
2024-10-02 12:13:05 +0100

    Add files via upload

---
+++ scripttugames.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.nav-buttons a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+        });
+    });
+});
+// JavaScript Document
